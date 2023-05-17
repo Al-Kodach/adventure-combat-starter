4044bffd5f3998d62857470536bf4245cf15c533
Al-kodach <alkodach351@gmail.com>
2023-05-17 05:39:12 -0400

    Code-refactor

--- starter/class/enemy.js
+++ starter/class/enemy.js
@@ -1,49 +1,78 @@
-const {Character} = require('./character');
-
+const { Character } = require("./character");
 
 class Enemy extends Character {
   constructor(name, description, currentRoom) {
-    // Fill this in
+    super(name, description, currentRoom);
+    this.cooldown = 3000;
+    this.attackTarget = null;
   }
 
   setPlayer(player) {
     this.player = player;
   }
 
-
   randomMove() {
-    // Fill this in
+    // get all exits path in current room
+    // select a room at room from exits rooms
+    // move enemy to that room.
+
+    let exits = this.currentRoom.getExits();
+    let randomExit = exits[Math.floor(Math.random() * exits.length)];
+    let nextRoom = this.currentRoom.getRoomInDirection(randomExit);
+    this.currentRoom = nextRoom;
+
+    this.cooldown += 1000;
+    this.act();
+    this.attackTarget = null;
   }
 
   takeSandwich() {
-    // Fill this in
+    // at every room, we check for sandwich
+    let food = this.currentRoom.getItemByName(sandwich);
+
+    if (food) {
+      this.currentRoom.items.splice(
+        this.currentRoom.items.indexOf(food), 1
+      );
+      this.items.push(food);
+      this.health += 10;
+    }
   }
 
   // Print the alert only if player is standing in the same room
   alert(message) {
-    if (this.player && this.player.currentRoom === this.currentRoom) {
-      console.log(message);
+    if (this.player) {
+      if (this.player.currentRoom === this.currentRoom) {
+        this.alert(message);
+      }
     }
   }
 
   rest() {
     // Wait until cooldown expires, then act
-    const resetCooldown = function() {
+    const resetCooldown = function (){
       this.cooldown = 0;
-      this.act();
     };
-    setTimeout(resetCooldown, this.cooldown);
+    setTimeout(() => resetCooldown, this.cooldown);
   }
 
   attack() {
-    // Fill this in
+    this.attackTarget.applyDamage(this.strength);
+    console.log(`The ${this.name} hit you for ${this.strength} damage.`);
+    this.cooldown += 3000;
+    this.act();
   }
 
   applyDamage(amount) {
-    // Fill this in
+    this.health -= amount;
+    if (this.health <= 0) {
+      this.die();
+      console.log(`The ${this.name} died.`);
+    } else {
+      this.attackTarget = this.player;
+      this.act();
+    }
   }
-
-
 
   act() {
     if (this.health <= 0) {
@@ -55,18 +84,14 @@ class Enemy extends Character {
       this.rest();
     }
 
-    // Fill this in
+    setTimeout(() => this.scratchNose, 3000);
   }
-
 
   scratchNose() {
     this.cooldown += 1000;
-
+    this.act();
     this.alert(`${this.name} scratches its nose`);
-
   }
-
-
 }
 
 module.exports = {
